59540cf8f92c6e5a1b4e330f6301c9ce61e2e365
himanshuuniyal200430 <himanshuuniyal40@gmail.com>
2026-07-08 12:41:40 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ const app = express();
 
 // Middleware
 const allowedOrigins = [
-  'https://trekking-2rih.onrender.com',
+  'https://matrikatoursandtravels.onrender.com',
   process.env.FRONTEND_URL,
   'http://localhost:5173',
   'http://localhost:5174',
